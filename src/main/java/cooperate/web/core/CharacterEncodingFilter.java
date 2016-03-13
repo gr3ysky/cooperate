efba361f0c7109a75bad9cf59ef5a1cfbf12b67f
gr3ysky <taneryigit@live.com>
2016-03-13 16:25:50 +0200

    sql dumb

--- src/main/java/cooperate/web/core/CharacterEncodingFilter.java
+++ src/main/java/cooperate/web/core/CharacterEncodingFilter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package cooperate.web.core;
 
 import javax.servlet.*;
@@ -20,27 +19,4 @@ public class CharacterEncodingFilter implements Filter {
 
     public void destroy() {
     }
-=======
-package cooperate.web.core;
-
-import javax.servlet.*;
-import java.io.IOException;
-
-
-public class CharacterEncodingFilter implements Filter {
-
-    public void init(final FilterConfig filterConfig) throws ServletException {
-    }
-
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        try {
-            response.setCharacterEncoding("UTF-8");
-        } finally {
-            chain.doFilter(request, response);
-        }
-    }
-
-    public void destroy() {
-    }
->>>>>>> github/master
 }
